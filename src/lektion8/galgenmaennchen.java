4ab85101651c56c63b72f0bd851d3b5bb71c47dc
MKCode1337 <max.b.kropf@gmail.com>
2025-11-24 11:28:03 +0100

    Lektionen 1-8 (Galgenmaennchen)

--- src/lektion8/galgenmaennchen.java
+++ src/lektion8/galgenmaennchen.java
@@ -10,22 +10,37 @@ public class galgenmaennchen {
         char[] rateWort = wort[zufallsIndex].toCharArray();
         System.out.println(Arrays.toString(rateWort));
         char[] eingabeN = new char[15];
-
+        char[] loesung = new char[rateWort.length];
+        int versucheCounter=0;
         for (int i = 0; i < 15; i++) {
-            wortAnzeigen(rateWort, eingabeN);
+            // Wort anzeigen
+            for (int n = 0; n < rateWort.length; n++) {
+                char ausgabe = '_';
+                for (int j = 0; j < eingabeN.length; j++) {
+                    if (rateWort[n] == eingabeN[j]) {
+                        ausgabe = rateWort[n];
+                        loesung[n] = rateWort[n];
+                        break;
+                    }
+                }
+                System.out.print(ausgabe+" ");
+            }
             System.out.println("\n");
-            eingabeN[i] = eingabeLesen();
+
 
             //Gewinnbedingung prüfen
             String loesungswort = String.valueOf(rateWort);
-            String eingabeWort = String.valueOf(eingabeN);
+            String eingabeWort = String.valueOf(loesung);
             if (loesungswort.equals(eingabeWort)) {
                 System.out.println("Gewonnen!!!");
                 break;
             }
+            else eingabeN[i] = eingabeLesen();
+            versucheCounter++;
         }
-
+        if (versucheCounter == 15) System.out.println("Leider verloren!!!");
     }
+
     public static char eingabeLesen() {
         Scanner input = new Scanner(System.in);
         System.out.println("Buchstabe eingeben: ");
@@ -33,18 +48,18 @@ public class galgenmaennchen {
         return eingabe.toUpperCase().charAt(0);
     }
 
-    public static void wortAnzeigen(char[] rateWort, char[] eingabeN) {
-        for (int i = 0; i < rateWort.length; i++) {
-            char ausgabe = '_';
-            for (int j = 0; j < eingabeN.length; j++) {
-                if (rateWort[i] == eingabeN[j]) {
-                    ausgabe = rateWort[i];
-                    break;
-                }
-            }
-            System.out.print(ausgabe+" ");
-        }
-    }
+//    public static void wortAnzeigen(char[] rateWort, char[] eingabeN) {
+//        for (int i = 0; i < rateWort.length; i++) {
+//            char ausgabe = '_';
+//            for (int j = 0; j < eingabeN.length; j++) {
+//                if (rateWort[i] == eingabeN[j]) {
+//                    ausgabe = rateWort[i];
+//                    break;
+//                }
+//            }
+//            System.out.print(ausgabe+" ");
+//        }
+//    }
 
 
 }
